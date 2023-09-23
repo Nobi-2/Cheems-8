c462b56b291f5beac9d386d79229349205d32699
ɳσႦιƚα <127091256+Nobi-2@users.noreply.github.com>
2023-09-24 00:59:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['917000562148'] //ur owner number
+global.ownernomer = "919174493135" //ur owner number2
+global.ownername = "ɳσႦιƚα 🐈‍⬛" //ur owner name
+global.ytname = "YT: nobi" //ur yt chanel name
+global.socialm = "GitHub: nobi-2" //ur github or insta name
+global.location = "India, " //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
+global.botname = "ɳσႦι🐈‍⬛"
 global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.ownername = 'ɳσႦιƚα🐈‍⬛'
+global.ownerNumber = ["91700562148@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@badnobita776"
+global.websitex = "GitHub.com/nobi-2"
+global.wagc = "https://chat.whatsapp.com/FnMfdfQADzZKrT0wQCSjGK"
+global.themeemoji = '🐈‍⬛'
+global.wm = "ɳσႦιƚα🖤."
+global.botscript = 'https://github.com/nobi-2/cheems8' //script link
+global.packname = "ɳσႦιƚα🐈‍⬛"
+global.author = "ɳσႦιƚα 🖤\n\n"
+global.creator = "917000562148@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
@@ -71,3 +71,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 })
+
