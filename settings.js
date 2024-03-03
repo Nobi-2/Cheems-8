746b6babec339db9132acae1e4623ab5408b1937
ɳσႦιƚα <127091256+Nobi-2@users.noreply.github.com>
2024-03-03 14:11:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,23 +21,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['919174493135','919179603804','8801922024324'] //ur owner number
-global.ownernomer = "919174493135" //ur owner number2
-global.ownername = "Nusi🐈‍⬛" //ur owner name
-global.ytname = "YT: nobi" //ur yt chanel name
-global.socialm = "GitHub: nobi-2" //ur github or insta name
+global.owner = ['919174493135','917440699135','919179603804','8801922024324'] //ur owner number
+global.ownernomer = "917440699135" //ur owner number2
+global.ownername = "ƙµɾơ🐈‍⬛" //ur owner name
+global.ytname = "YT: kuro" //ur yt chanel name
+global.socialm = "GitHub: kuro-2" //ur github or insta name
 global.location = "India, " //ur location
 
 //new
-global.botname = "ɳusi 🐈‍⬛"
-global.ownernumber = '919174493135'
-global.ownername = 'ɳσႦιƚα🐈‍⬛'
-global.ownerNumber = ["919174493135@s.whatsapp.net"]
+global.botname = "ƙµɾơ 🐈‍⬛"
+global.ownernumber = '917440699135'
+global.ownername = 'ƙµɾơ🐈‍⬛'
+global.ownerNumber = ["917440699135@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@badnobita776"
 global.websitex = "GitHub.com/nobi-2"
 global.wagc = "https://chat.whatsapp.com/BvXWEFsHfJA1guxS9JYn8C"
 global.themeemoji = '🐈‍⬛'
-global.wm = "ɳσႦιƚα🖤."
+global.wm = "ƙµɾơ 🖤."
 global.botscript = 'https://github.com/nobi-2/cheems8' //script link
 global.packname = "LiSa"
 global.author = "🤍\n\n"
